e81575a20da3c90c45f0a2ac8ff56aec4d7d1837
rootid <vsinhsawant@gmail.com>
2015-01-27 17:24:44 -0500

    find common element

---
+++ pie/findCommonEleFromSet.cpp
@@ -0,0 +1,11 @@
+
+//find the repeated elements inside the set
+int findCommanElementFromSet () {
+    
+    //Use of BV : before that 
+                //1.Run selection algorithm to seperate out +ve and -ve numbers
+                //2.Run bitwise AND algorithm for positive numbers
+                //3.Run bitwise AND algorithm for negative algorithms
+                //4.output both positive and negative number
+    
+} 
